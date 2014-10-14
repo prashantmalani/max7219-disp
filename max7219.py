5d8cd447d8d8ac1151c8f5d520e388eb425764c7
Prashant Malani <p.malani@gmail.com>
2014-10-13 23:16:49 -0700

    The Test mode works

--- max7219.py
+++ max7219.py
@@ -9,8 +9,105 @@
 # Vcc - Pin 2 (5V)
 # Gnd - Pin 9 (GND)
 # Din - Pin 11 (GPIO 17)
-# CS  - Pin 15 (GPIO 22)
+# CS_BAR  - Pin 15 (GPIO 22)
 # Clk - Pin 16 (GPIO 23)
 #
 import RPi.GPIO as GPIO
 import time
+
+# Local defines (GPIO mappings)
+DIN = 17
+CS_BAR = 22
+CLK = 23
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(DIN, GPIO.OUT)
+GPIO.setup(CS_BAR, GPIO.OUT)
+GPIO.setup(CLK, GPIO.OUT)
+
+
+GPIO.output(CS_BAR, GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(DIN, GPIO.HIGH)
+time.sleep(0.002)
+
+# First 4 bits don't care
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+
+# 0xF address
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+
+#D7-D1 don't care, D0=1
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.LOW)
+time.sleep(0.002)
+GPIO.output(CLK,GPIO.HIGH)
+time.sleep(0.002)
+
+GPIO.output(CS_BAR, GPIO.HIGH)
+
+print "Done all the GPIO stuff..."
+
+time.sleep(5)
+
+
+GPIO.cleanup()
+
