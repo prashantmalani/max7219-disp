3f16fcb4567e00662bbe581b0c5d9a6934829566
Prashant Malani <p.malani@gmail.com>
2014-10-23 00:02:23 -0700

    Move print string code into driver

--- max7219.py
+++ max7219.py
@@ -72,6 +72,24 @@ class Max7219:
             row_addr += 1
         self.writeData(row_addr, 0)
 
+    def writeString(self, string):
+        """ Print provided string in the form of a scrolling text.
+        """
+        screen_data = ltrs.char_map[string[0]][:]
+        self.writeScreen(screen_data)
+        time.sleep(2)
+        for cur_char in string[1:]:
+            cur_data = ltrs.char_map[cur_char][:]
+            # Run the inner loop 8 times to get through all 8 columns
+            for i in range(0, 8):
+                # Left shift each row, and then update it
+                for i in range(0, 8):
+                    new_bit = (cur_data[i] >> 7) & 1
+                    screen_data[i] = ((screen_data[i] << 1) | new_bit) & 0xFF
+                    cur_data[i] = (cur_data[i] << 1) & 0xFF
+                self.writeScreen(screen_data)
+                time.sleep(0.1)
+        time.sleep(1)
 
     def cleanUp(self):
         self.writeData(0xC, 0x0)
@@ -79,6 +97,7 @@ class Max7219:
         GPIO.cleanup()
 
 if __name__ == "__main__":
+    # Some sample code
     max_drv = Max7219()
     max_drv.initialize()
     test1 = [0x1, 0x3, 0x7, 0xF, 0x1F, 0x3F, 0x7F, 0xFF]
--- printstring.py
+++ printstring.py
@@ -12,26 +12,10 @@ import time
 
 
 if __name__ == "__main__":
-    sample_str = "BAD"
+    sample_str = "BAD BACKED"
     max_drv = max7219.Max7219()
     max_drv.initialize()
-    # Do awesome stuff here
-    # Load the screen data with the values to be initially displayed
-    screen_data = ltrs.char_map[sample_str[0]][:]
-    max_drv.writeScreen(screen_data)
-    time.sleep(2)
-    for cur_char in sample_str[1:]:
-        cur_data = ltrs.char_map[cur_char][:]
-        # Run the inner loop 8 times to get through all 8 columns
-        for i in range(0, 8):
-            # Left shift each row, and then update it
-            for i in range(0, 8):
-                new_bit = (cur_data[i] >> 7) & 1
-                screen_data[i] = ((screen_data[i] << 1) | new_bit) & 0xFF
-                cur_data[i] = (cur_data[i] << 1) & 0xFF
-            max_drv.writeScreen(screen_data)
-            time.sleep(0.3)
-    time.sleep(1)
+    max_drv.writeString("BAD BACKED")
     max_drv.cleanUp()
 
 
